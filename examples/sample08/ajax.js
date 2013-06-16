0cdd29c885c29bc6a1fe2d4b51a9db59d46868e2
Yoshi <gpp65536@yahoo.co.jp>
2013-06-16 16:24:10 +0900

    examples/sample08/ajax.js: modified indent

--- examples/sample08/ajax.js
+++ examples/sample08/ajax.js
@@ -7,12 +7,12 @@
 
 function getRotationArray (id, src, dst, type) {
 	if(typeof type === 'undefined') type = 'default';
-    $.ajax({
-        url: "../../lib/cgi/get_rotation_array.cgi?src="+src+"&dst="+dst+"&id="+id,
-        type: "GET",
-        data: {},
-        contentType: "application/json",
-        dataType: "json",
+	$.ajax({
+		url: "../../lib/cgi/get_rotation_array.cgi?src="+src+"&dst="+dst+"&id="+id,
+		type: "GET",
+		data: {},
+		contentType: "application/json",
+		dataType: "json",
 		success: function(msg) {  
 			if (msg['ret'] != 0) return 0;
 			if (type == 'gw') 
@@ -20,43 +20,42 @@ function getRotationArray (id, src, dst, type) {
 			else 
 				g.drawinfo_flows[msg['id']]['rot'] = msg['rot'];
 		},
-        error: function() { $("#debug").append(
+		error: function() { $("#debug").append(
 			"Error: getRotationArray(): failed to get a rotation array."); },
-        complete: undefined
-    });
-
+		complete: undefined
+	});
 }
 
 function getSdnObjects(key) {
 	var datapath = "./get_objs.cgi?id="+key;
 	g.sdn_objs = {};
-    $.ajax({
-        url: datapath,
-        type: "GET",
-        data: {},
-        contentType: "application/json",
-        dataType: "json",
+	$.ajax({
+		url: datapath,
+		type: "GET",
+		data: {},
+		contentType: "application/json",
+		dataType: "json",
 		success: function(msg) { g.sdn_objs = msg; },
-        error: function() { $("#debug").append(
+		error: function() { $("#debug").append(
 			"Error: loadObject(): failed to get object parameters"); },
-        complete: undefined
-    });
+		complete: undefined
+	});
 }
 
 function getOtherObjects(key) {
 	g.other_objs = {};
 	if (key != 4) return; 
-    $.ajax({
-        url: "../../lib/cgi/get_otherobjs.cgi",
-        type: "GET",
-        data: {},
-        contentType: "application/json",
-        dataType: "json",
+	$.ajax({
+		url: "../../lib/cgi/get_otherobjs.cgi",
+		type: "GET",
+		data: {},
+		contentType: "application/json",
+		dataType: "json",
 		success: function(msg) { g.other_objs = msg; },
-        error: function() { $("#debug").append(
+		error: function() { $("#debug").append(
 			"Error: loadObject(): failed to get object parameters"); },
-        complete: undefined
-    });
+		complete: undefined
+	});
 }
 
 function loadObject () {
@@ -68,35 +67,29 @@ function loadObject () {
 function getFlowData (key) {
 	if (key == undefined) key = g.flowdata_id;
 	g.flowinfo = {};
-    $.ajax({
-        url: "get_flow.cgi?id="+key,
-        type: "GET",
-        data: {},
-        contentType: "application/json",
-        dataType: "json",
-		success: function(msg) { 
-			g.flowinfo = msg;
-		},
-        error: function() { $("#debug").append(
+	$.ajax({
+		url: "get_flow.cgi?id="+key,
+		type: "GET",
+		data: {},
+		contentType: "application/json",
+		dataType: "json",
+		success: function(msg) { g.flowinfo = msg; },
+		error: function() { $("#debug").append(
 			"Error: loadObject(): failed to get flow lists"); },
-        complete: undefined
-    });
+		complete: undefined
+	});
 
 	if (g.gwflowstat != undefined) return;
 	g.gwflowstat = {};
-    $.ajax({
-        url: "../../lib/cgi/get_gwflow_dummy.cgi",
-        type: "GET",
-        data: {},
-        contentType: "application/json",
-        dataType: "json",
-		success: function(msg) { 
-			g.gwflowstat = msg;
-		},
-        error: function() { $("#debug").append(
+	$.ajax({
+		url: "../../lib/cgi/get_gwflow_dummy.cgi",
+		type: "GET",
+		data: {},
+		contentType: "application/json",
+		dataType: "json",
+		success: function(msg) { g.gwflowstat = msg; },
+		error: function() { $("#debug").append(
 			"Error: loadObject(): failed to get gateway flow lists"); },
-        complete: undefined
-    });
+		complete: undefined
+	});
 }
-
-
